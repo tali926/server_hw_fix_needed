514ec891fea85d762a07cff10ababe4c5638809a
tali926 <Hand1e10@mbjcomp.org.il>
2025-10-31 10:38:49 +0200

    Add files via upload

---
+++ lesson_5_books/app.ts
@@ -0,0 +1,74 @@
+import express, {Express, Request, Response} from "express";
+import { Books } from "./books";
+
+const HOST = "127.0.0.1";
+const PORT= 5000;
+
+export default class App{
+    private app: express;
+    private arr : Array <Books>;
+
+
+    constructor(){
+        this.app= express();
+        this.arr= [];
+    }
+    public init() {
+            this.app.use(express.json()); 
+            this.setRoutes();
+    
+            this.app.listen(PORT, HOST, () => {
+                console.log(`Server is listening on: http://${HOST}:${PORT}`);
+            });
+    
+        }
+
+        private setRoutes(){
+        this.app.get("/api/books", (req: Request, res: Response) => {
+            res.send(this.arr);
+        });
+
+        this.app.get("/api/books/:code", (req: Request, res: Response)=> {
+            const code= parseInt(req.params.code);
+
+          const book = this.arr.find(b => b.code == code);
+            if (!book) {
+                res.status(404).send("book was not found");
+            }
+            res.send(book);
+
+        });
+
+        this.app.post("/api/books",(req: Request, res: Response)=>{
+            const books: Books= req.body;
+            this.arr.push(books);
+            res.status(201);
+        });
+
+    this.app.put("/api/books/:code", (req: Request, res: Response) => {
+            const code = parseInt(req.params.code); 
+            const bookIndex = this.arr.findIndex(b => b.code == code);
+            if (bookIndex == -1) {
+                res.status(404).send("book was not found");
+            }
+
+            const book: Books = req.body;
+           
+
+            this.arr[bookIndex] = book;
+            res.end();
+        });
+
+        this.app.delete("api/books/:code", (req: Request, res: Response) => {
+             const code= parseInt(req.params.code);
+              const bookIndex = this.arr.findIndex(b => b.code == code);
+            if (bookIndex == -1) {
+                res.status(404).send("book was not found");
+            }
+
+        this.arr.splice(bookIndex,1);
+       res.send(arr);
+        })
+}
+
+}
